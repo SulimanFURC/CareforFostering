063882a56f3e59371be9f90e72f9d59dde3ad313
Mehdi Bukhari <mehdi@maservices-ltd.com>
2020-12-24 15:41:13 +0500

    inital commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,40 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-about-us-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\about-us.js"))),
+  "component---src-pages-big-data-analysis-startup-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\big-data-analysis-startup.js"))),
+  "component---src-pages-blog-details-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\blog-details.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\blog.js"))),
+  "component---src-pages-case-studies-details-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\case-studies-details.js"))),
+  "component---src-pages-case-studies-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\case-studies.js"))),
+  "component---src-pages-coming-soon-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\coming-soon.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\contact.js"))),
+  "component---src-pages-course-details-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\course-details.js"))),
+  "component---src-pages-courses-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\courses.js"))),
+  "component---src-pages-data-analytics-ai-startup-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\data-analytics-ai-startup.js"))),
+  "component---src-pages-data-analytics-ml-consulting-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\data-analytics-ml-consulting.js"))),
+  "component---src-pages-data-science-ml-company-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\data-science-ml-company.js"))),
+  "component---src-pages-data-science-online-courses-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\data-science-online-courses.js"))),
+  "component---src-pages-digital-marketing-agency-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\digital-marketing-agency.js"))),
+  "component---src-pages-event-details-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\event-details.js"))),
+  "component---src-pages-events-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\events.js"))),
+  "component---src-pages-faq-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\faq.js"))),
+  "component---src-pages-gallery-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\gallery.js"))),
+  "component---src-pages-history-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\history.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\index.js"))),
+  "component---src-pages-machine-learning-ai-solutions-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\machine-learning-ai-solutions.js"))),
+  "component---src-pages-membership-levels-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\membership-levels.js"))),
+  "component---src-pages-privacy-policy-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\privacy-policy.js"))),
+  "component---src-pages-profile-authentication-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\profile-authentication.js"))),
+  "component---src-pages-seo-agency-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\seo-agency.js"))),
+  "component---src-pages-service-details-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\service-details.js"))),
+  "component---src-pages-services-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\services.js"))),
+  "component---src-pages-team-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\team.js"))),
+  "component---src-pages-terms-of-service-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\terms-of-service.js"))),
+  "component---src-pages-testimonials-js": hot(preferDefault(require("D:\\Oracalo\\Project Fostering\\alpha site\\src\\pages\\testimonials.js")))
+}
+
